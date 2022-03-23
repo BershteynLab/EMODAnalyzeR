68ff8e9726f13c0dcddc52416e6be77f8c384e8b
Kaftan <kaftad01@nyumc.org>
2022-03-23 10:09:19 -0400

    read prevalence data only once

--- R/ReportEMODResults.R
+++ R/ReportEMODResults.R
@@ -12,32 +12,59 @@ agebin.to.min.max <- function(agebin) {
 #'
 #' @param experiment_path path on big purple where experiment was run. Typically this will look like
 #' /gpfs/scratch/kaftad01/experiments/Zambia--0.02--rep7--testCalibrateSeedYear_iter32___2022_03_21_12_39_07_01727
-report.calibration.results <- function(experiment_path, ingest_path) {
+report.calibration.results <- function(experiment_path, ingest_path, figure_path = "./") {
   ingest_data <- read.ingest.file(ingest_path)
   plottopics.prevalence <- ingest_data[['Obs-Prevalence']] %>%
                             select(Province, AgeBin) %>%
                             distinct() %>%
-                            agebin.to.min.max()
-  for (i_row in seq(1, nrow(plottopics.prevalence) - 1)){
-    actuals <- ingest_data[['Obs-Prevalence']] %>%
-                  filter(Province == plottopics.prevalence[[i_row,'Province']] &
-                           AgeBin == plottopics.prevalence[[i_row,'AgeBin']] )
-    data <- read.simulation.results.bigpurple(
-                                      experiment_path, "calibration",
-                                      min_age_inclusive = plottopics.prevalence$age.min[i_row],
-                                      max_age_inclusive = plottopics.prevalence$age.max[i_row] + 1,
-                                      stratify_columns = c("Year","NodeId", "Gender"),
-                                      summarize_columns = c("Population","Infected")) %>%
-            filter(ingest_data[["site_map"]][[plottopics.prevalence[[i_row,'Province']]]] == NodeId)
+                            agebin.to.min.max() %>%
+                            filter(
+                              (!is.na(Province)) &
+                                (!is.na(AgeBin)) )
 
-    plot.prevalence(data, 2000, 2025) +
-      geom_point(aes(x=Year, y=Prevalence)) +
-      geom_errorbar(data = actuals, aes(x=Year, ymin=lb, ymax=ub), color="black", width=2, size=1)
+  data <- read.simulation.results.bigpurple(
+    experiment_path, "calibration",
+    min_age_inclusive = -1,
+    max_age_inclusive = 200,
+    stratify_columns = c("Year","NodeId", "Gender", "Age"),
+    summarize_columns = c("Population","Infected"))
 
-    ggsave(paste0(plottopics.prevalence[[i_row,'Province']], plottopics.prevalence[[i_row,'age.min']], '-', plottopics.prevalence[[i_row,'age.max']], ".png"))
+  if (any("Obs-Prevalence" == names(ingest_data))) {
 
+    for (i_row in seq(1, nrow(plottopics.prevalence))) {
+      actuals <- ingest_data[['Obs-Prevalence']] %>%
+                    filter(Province == plottopics.prevalence[[i_row,'Province']] &
+                             AgeBin == plottopics.prevalence[[i_row,'AgeBin']] )
+
+
+      if ( str_to_lower(plottopics.prevalence[[i_row,'Province']]) == 'all' ) {
+        this.sim.data = data %>%
+          filter(Age > plottopics.prevalence$age.min[i_row] &
+                   Age < ( plottopics.prevalence$age.max[i_row] + 1 ) ) %>%
+          dplyr::group_by(Year, Gender, sim.id) %>%
+          dplyr::summarize(Population = sum(Population), Infected = sum(Infected))
+      } else {
+        this.sim.data = data %>%
+          filter(Age > plottopics.prevalence$age.min[i_row] &
+                   Age < ( plottopics.prevalence$age.max[i_row] + 1 ) &
+                   ingest_data[["site_map"]][[plottopics.prevalence[[i_row,'Province']]]] == NodeId) %>%
+          dplyr::group_by(Year, Gender, sim.id, scenario_name) %>%
+          dplyr::summarize(Population = sum(Population), Infected = sum(Infected))
+      }
+
+      plot.prevalence(this.sim.data, 2000, 2025) +
+        geom_point(data = actuals, aes(x=Year, y=Prevalence)) +
+        geom_errorbar(data = actuals, aes(x=Year, ymin=lb, ymax=ub), color="black", width=2, size=1)
+
+      ggsave(paste0(figure_path, '/',
+                    plottopics.prevalence[[i_row,'Province']],
+                    plottopics.prevalence[[i_row,'age.min']],
+                    '-',
+                    plottopics.prevalence[[i_row,'age.max']],
+                    "Prevalence.png"))
+
+    }
   }
-  plt
 }
 
 #d = report.calibration.results("C:/Users/kaftad01/Documents/EMOD/ReportHIVByAgeAndGender (1) - Copy", "D:/Dropbox (NYU CEDS)/EMOD HIV User Group Shared Folder/input_file_bundles/Nyanza_calibration/k20220218_Nyanza_baseline_from_PrEP/Data/calibration_ingest_form_Nyanza.xlsm")
