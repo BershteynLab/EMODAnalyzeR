00c5f8fc08016b31f34f7b26207bd148acb53251
Kaftan <kaftad01@nyumc.org>
2022-05-31 13:19:25 -0400

    fix fancy quotes

--- examples/DemoCostEffectiveness.R
+++ examples/DemoCostEffectiveness.R
@@ -2,7 +2,7 @@
 # Make sure you are running R 4.0 or greater
 # Make sure you have devtools installed (if not, run install.packages(“devtools”) )
 # Run library(devtools)
-# Run install_github(“BershteynLab/EMODAnalyzeR”)
+# Run install_github("BershteynLab/EMODAnalyzeR")
 
 library(EMODAnalyzeR)
 
@@ -47,7 +47,7 @@ baseline <- icer.run.comparison(data.intervention,
                            data.nointervention,
                            0.03,
                            "Baseline",
-                           2055,
+                           end_year = 2055,
                            name.treatment_population_column = on_treatment_col_name,
                            name.treatment_initiation_column = treatment_initiated_col_name )
 
