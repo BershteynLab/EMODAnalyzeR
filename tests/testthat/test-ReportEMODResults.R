e1a320442a2dbfd960fc8531613355819dc869ac
Kaftan <kaftad01@nyumc.org>
2022-11-02 14:10:41 -0400

    fix test data for test TODO: make reporting flexible to report ignore provinces in ingest that dont exit

---
+++ tests/testthat/test-ReportEMODResults.R
@@ -0,0 +1,16 @@
+test_that("report.calibration.results doesnt error", {
+# insert path to your reportbyageandgender files
+path_to_report_by_age_and_gender <- "../test_data/ReportHIVByAgeAndGender"
+
+# insert path to ingest file. For Nyanza, you can download from https://www.dropbox.com/s/wkylqakjxkyzffb/calibration_ingest_form_Nyanza.xlsm?dl=0
+# ingest file contains data used for calibration (prevalence, etc)
+path_to_ingest_file <- "../test_data/calibration_ingest_form_Nyanza.xlsm"
+
+# insert path to where you want the figure files to appear
+figure_output_folder <- "./"
+
+# population scaling factor
+pop_scaling_factor = 0.002
+
+report.calibration.results(path_to_report_by_age_and_gender, path_to_ingest_file, figure_path = figure_output_folder, pop_scaling_factor = pop_scaling_factor)
+})
